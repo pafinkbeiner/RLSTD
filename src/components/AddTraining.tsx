499d66c97df79b1eb32fbde4e323e68278eee2ab
pafinkbeiner <paul-d-f@web.de>
2024-10-20 12:00:34 +0200

    sc

--- src/components/AddTraining.tsx
+++ src/components/AddTraining.tsx
@@ -15,6 +15,7 @@ import { Metric, Training } from "@/types/Training";
 import { Subject } from "rxjs";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
+import { Slider } from "./ui/slider";
 
 export function AddTraining() {
 
@@ -23,10 +24,10 @@ export function AddTraining() {
   const [trainingTitle, setTrainingTitle] = useState<string>("");
 
   const [chartData, setChartData] = useState<Array<Metric>>([
-    { ts: 1, target: 100 },
-    { ts: 2, target: 200 },
-    { ts: 3, target: 150 },
-    { ts: 4, target: 100 },
+    { ts: 0, target: 100 },
+    { ts: 1, target: 200 },
+    { ts: 2, target: 150 },
+    { ts: 3, target: 100 },
   ])
 
   const addChartData = (v: Metric) => {
@@ -61,7 +62,7 @@ export function AddTraining() {
     const training: Training = {
       id: crypto.randomUUID(),
       title: trainingTitle,
-      targetedTrainingTime: chartData[chartData.length - 1].ts,
+      targetedTrainingTime: chartData[chartData.length - 1].ts + 1,
       targetPowerZones: chartData,
       cloudSynchronised: false
     }
@@ -71,6 +72,12 @@ export function AddTraining() {
     setTimeStamp(0);
     setTargetPower(0);
     setTrainingTitle("");
+    setChartData([
+      { ts: 0, target: 100 },
+      { ts: 1, target: 200 },
+      { ts: 2, target: 150 },
+      { ts: 3, target: 100 },
+    ]);
   }
 
   return (
@@ -88,18 +95,27 @@ export function AddTraining() {
         <div className="flex flex-col gap-3 pt-5 pb-5">
           <AddTrainingAreaChart chartData={chartData} setTargetPower={setTargetPower} setTimeStamp={setTimeStamp} />
 
-          <div className="flex flex-col gap-2 w-64">
+          <div className="flex flex-col gap-2 w-96">
             <div className="flex flex-row gap-5 items-center">
               <Label>Trainings titel</Label>
               <Input type="string" value={trainingTitle} onChange={(e) => setTrainingTitle(e.target.value)} />
             </div>
             <div className="flex flex-row gap-5 items-center">
               <Label>Timestamp</Label>
-              <Input type="number" value={timeStamp} onChange={(e) => setTimeStamp(parseInt(e.target.value))} />
+              <Input type="number" value={timeStamp} onChange={(e) => {
+                const value = parseInt(e.target.value);
+                setTimeStamp(value);
+                // if (!isNaN(value)) {
+                  
+                // }
+              }} />
             </div>
             <div className="flex flex-row gap-5 items-center">
               <Label>Target Power</Label>
-              <Input type="number" value={targetPower} onChange={(e) => setTargetPower(parseInt(e.target.value))} />
+              <div className="flex flex-row gap-3 w-full">
+                <Slider onValueChange={(e) => setTargetPower(e[0])} defaultValue={[targetPower]} value={[targetPower]} max={1000} step={10} />
+                ({targetPower})
+              </div>
             </div>
           </div>
 
